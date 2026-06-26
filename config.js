5f75e83d85ba2f61bdd9df7af9e7ba8f5da79836
anthonyabiaad <165178688+anthonyabiaad@users.noreply.github.com>
2026-06-26 13:53:23 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -9,41 +9,4 @@ const CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQlZB_yrUvpRD2Y
 
 const WHATSAPP_URL = "https://chat.whatsapp.com/Jg8feDmGYYs3R0Xxuo2sx2?mode=gi_t";
 
-const MEMBER_MESSAGE = `Hello Anthony, I would like to be added to the Lebanese Physics Society member directory.
-
-Name:
-Name FAMILY NAME
-
-Branch/Keywords about your research:
-Theory "T", Experimental "E", Numerical "N", a mixed: TE, EN, ... and keywords about your research field
-
-Contact (mail/phone number):
-Any type of contact if someone wants to talk to you
-
-PhD lab/institution:
-Where you did your PhD
-
-MSc institution:
-Where you did your master degree
-
-BSc institution:
-Where you did your bachelor degree
-
-Notable contacts (PhD/past interns/supervisors, ...):
-People that you worked with or supervised you during your research/studies
-
-LinkedIn link:
-A link into your LinkedIn profile, if available
-
-Current position:
-Your current position
-
-Details:
-Any detail you'd like to add`;
-
-const IS_MOBILE = /Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
-
-const MEMBER_FORM_URL =
-  (IS_MOBILE ? "https://api.whatsapp.com/send" : "https://web.whatsapp.com/send") +
-  "?phone=33662436766&text=" +
-  encodeURIComponent(MEMBER_MESSAGE);
+const MEMBER_FORM_URL = "https://docs.google.com/forms/d/e/1FAIpQLSdez2OR9SmVweZJho6o7KaV-Omo5QQbL2-6aqIfwBq_wwd6lA/viewform?usp=publish-editor";
